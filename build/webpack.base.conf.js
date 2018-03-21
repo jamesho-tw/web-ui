ddbdcedf7d5046a6524e831f0c8f9f536b0242c1
jamesho.tw <jamesho.tw@gmail.com>
2018-03-21 17:18:19 +0800

    Add "axios" and "moment"

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,6 +1,7 @@
 'use strict'
 const path = require('path')
 const utils = require('./utils')
+const webpack = require('webpack')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
@@ -78,5 +79,10 @@ module.exports = {
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      axios: 'axios'
+    })
+  ]
 }
